7f79968ffcbbb756d53930910aefed13c01d4386
Rashid <stylorashid786@gmail.com>
2023-11-11 09:49:09 +0530

    clean Problem5 and Problem5

--- problem5.js
+++ problem5.js
@@ -1,10 +1,6 @@
-const inventory = require('./invertoryData')
-
 const carBefore2000 = (alotCarYear) => {
-  const yearOfCar =  alotCarYear.filter((car) => car < 2000);
+  const yearOfCar = alotCarYear.filter((car) => car < 2000);
   return yearOfCar;
-  
 };
-
 
 module.exports = carBefore2000;
--- problem6.js
+++ problem6.js
@@ -1,5 +1,3 @@
-const inventory = require("./invertoryData");
-
 const BMWandAudi = (inventory) => {
   return inventory.filter((inv) => {
     return inv.car_make === "Audi" || inv.car_make === "BMW";
